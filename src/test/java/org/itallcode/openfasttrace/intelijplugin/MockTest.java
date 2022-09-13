369740e59045b898e9fa81ed0e3265a2a45cbb5f
Sebastian Bär <sebastian.baer@exasol.com>
2022-09-13 07:59:30 +0200

    #3: Added mock test to get a test report generated.

---
+++ src/test/java/org/itallcode/openfasttrace/intelijplugin/MockTest.java
@@ -0,0 +1,10 @@
+package org.itallcode.openfasttrace.intelijplugin;
+
+import org.junit.Test;
+
+public class MockTest {
+    @Test
+    public void testNothing(){
+        // empty on purpose.
+    }
+}
